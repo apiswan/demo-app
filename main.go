84ca705d9b8961477fb90fc980e65e6a73ba00d9
apiswan <138169260+apiswan@users.noreply.github.com>
2023-12-21 22:48:46 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+type Chat struct {
+	Message string `json:"message" binding:"required"`
+	Room    string `json:"room" binding:"required"`
+}
+
+func main() {
+	// Create a new Gin router
+	r := gin.Default()
+
+	var chat = map[string]interface{}{
+		"data": map[string]interface{}{
+			"username":  "luffy",
+			"message":   "what's up guys!",
+			"timestamp": "532356",
+		},
+	}
+
+	// Handle GET request
+	r.GET("/chat-services/user/chat", func(c *gin.Context) {
+		c.JSON(http.StatusOK, chat)
+	})
+
+	// Handle POST request
+	r.POST("/chat-services/user/chat", func(c *gin.Context) {
+		var chat Chat
+		if err := c.BindJSON(&chat); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid data"})
+			return
+		}
+		c.JSON(http.StatusOK, map[string]string{"id": uuid.New().String(), "message": chat.Message, "room": chat.Room})
+	})
+
+	// Run the server on port 8080
+	r.Run(":8080")
+}
